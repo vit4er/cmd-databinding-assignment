cc27d2f4744c752474095c27e8c304248fe9c2a6
vit4er <vchru@yahoo.com>
2019-05-08 11:26:41 +0300

    Seems ready...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cmp-databinding-assignment';
+  num: number;
+  onNumberGenerated(data: {num: number}) {
+    this.num = data.num;
+  }
 }
